f4a65faf12923cea2c43792dc9042fb65fbfa2f5
Joe Witcpalek <joe.witcpalek@gmail.com>
2023-06-19 13:42:07 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = ['static']
 html_logo = "Team302LogoNew.png"
 
 # -- Options for EPUB output
